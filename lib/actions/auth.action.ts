ae015b85ce28242ae187a3f871d2467a143e5a43
Arnab Bhattacharya <arnabbhattacharya2005@gmail.com>
2025-05-08 19:15:24 +0530

    show generated interviews on home page

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { interviewCovers } from "@/constants";
 import { auth, db } from "@/firebase/admin";
 import { cookies } from "next/headers";
 const ONE_WEEK = 60*60*24*7;
@@ -101,4 +102,37 @@ export async function isAuthenticated(){
     const user = await getCurrentUser();
     if(!user) return false;
     return !!user;
+}
+
+export async function getInterviewByUserid(userId: string): Promise<Interview[] | null>{
+    const Interviews = await db
+                        .collection('interviews')
+                        .where('userId', '==', userId)
+                        .orderBy('createdAt', 'desc')
+                        .get();
+
+    return Interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+
+    })) as Interview[];
+}
+
+export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null>{
+
+    const { userId, limit=20 } = params;
+
+    const Interviews = await db
+                        .collection('interviews')
+                        .orderBy('createdAt', 'desc')
+                        .where('finalized', '==', true)
+                        .where('userId', '!=', userId)
+                        .limit(limit)
+                        .get();
+
+    return Interviews.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+
+    })) as Interview[];
 }
